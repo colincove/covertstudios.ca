3233a5d24a6c39fc32cd0b8523b0a1fbeee5b658
ColinCove <cove.colin@gmail.com>
2012-08-11 20:00:10 -0700

    Issues displaying Posts
    Posts were not being displayed correctly. Problem had to do with
    improper use of quotations after I made the switch from outputing a
    string in blog_entry to outputing html instead.

--- blog/blog_entry.php
+++ blog/blog_entry.php
@@ -17,7 +17,7 @@ echo " CURENTLY HIDDEN";
 <h5><?php echo formatDate($row['date']) ?></h5>
 <img src='content/<?php echo $row['thumbnail'] ?>_300x165.jpg' class='img_300x165 postThumbnail' align='left' />
 <img class='topicIcon' src='images/topicIcon.jpg' />
-<a class='postTitleLink' href='http://www.covertstudios.ca/blog/index.php?viewPost="<?php echo $row['uid'] ?>"'><h1><?php echo $row['title'] ?></h1></a>
+<a class='postTitleLink' href="http://www.covertstudios.ca/blog/index.php?viewPost=<?php echo $row['uid'] ?>"><h1><?php echo $row['title'] ?></h1></a>
 <div><?php echo $row['introduction'] ?></div>
 </div>
 
